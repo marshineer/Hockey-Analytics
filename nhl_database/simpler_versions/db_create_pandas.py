ab2cf3fe88523af18aa41bbe8ba18b7a49a4380c
Marshall Mykietyshyn <mmykietyshyn@gmail.com>
2022-12-01 02:31:28 +0100

    Moved to 'archived' directory

--- nhl_database/simpler_versions/db_create_pandas.py
+++ nhl_database/simpler_versions/db_create_pandas.py
@@ -4,13 +4,14 @@ from sqlalchemy import create_engine
 from nhl_database.db_common import get_file_names
 
 
+# Note: this solution does not create table relationships or primary/foreign keys
 # Initiate a database connection engine
 DATABASE_URL = 'postgresql+psycopg2://marshineer:password@localhost:5432/nhl_db'
 db_engine = create_engine(DATABASE_URL, echo=True)
 
 # Dynamically set the CWD
 froot = str(os.path.dirname(__file__))
-fpath = froot + '/../data/'
+fpath = froot + '/../../data/'
 tables = ['games', 'shots', 'skater_seasons', 'team_boxscores', 'coaches',
           'goalie_boxscores', 'shifts', 'skater_boxscores', 'goalie_seasons',
           'teams', 'players', 'game_events']
