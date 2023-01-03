212b0109a40ed0777edb688a38bca197a20f19c5
Marshall Mykietyshyn <mmykietyshyn@gmail.com>
2023-01-03 11:26:22 +0100

    Updated common

--- models/common.py
+++ models/common.py
@@ -1,4 +1,6 @@
 import numpy as np
+import psycopg2 as psy
+import pandas as pd
 
 
 # Modelling TODOs
@@ -43,3 +45,90 @@ def calc_travel_dist(city1, city2):
     return dist
 
 
+def create_db_connection(db, user, host, port, password=None):
+    """ Returns a database connection for executing sql commands.
+
+    Parameters
+        db: str = database name
+        user: str = username
+        host: str = host name (IP)
+        port: str = port number
+        password: str = password (optional)
+
+    Returns
+        connection = database connection object
+    """
+    connection = psy.connect(database=db,
+                             user=user,
+                             password=password,
+                             host=host,
+                             port=port)
+    return connection
+
+
+def get_col_names(conn, table):
+    """ Retrieve the column names for a particular database table.
+
+    Parameters
+        conn: database connection object
+        table: str = sql database table name
+
+    Returns
+        col_names: list = list of table column names
+    """
+
+    sql_query = f"""
+        SELECT *
+        FROM information_schema.columns
+        WHERE table_name = '{table}'
+        ORDER BY ordinal_position;
+        """
+
+    col_names = []
+    with conn.cursor() as cursor:
+        cursor.execute(sql_query)
+        col_data = cursor.fetchone()
+
+        while col_data is not None:
+            col_names.append(col_data[3])
+            col_data = cursor.fetchone()
+
+    return col_names
+
+
+def select_table(conn, table):
+    """ Retrieves an entire table from the database.
+
+    Parameters
+        conn: database connection object
+        table: str = table name
+
+    Returns
+        : data frame = table retrieved from database
+    """
+    cols = get_col_names(conn, table)
+    sql_query = f"""SELECT * FROM {table}"""
+    with conn.cursor() as cursor:
+        cursor.execute(sql_query)
+        data_list = cursor.fetchall()
+
+    return pd.DataFrame(data_list, columns=cols)
+
+
+def sql_select(conn, table, query):
+    """ Execute SQL SELECT command and returns pandas data frame.
+
+    Parameters
+        conn: database connection object
+        table: str = table name
+        query: str = SQL query command
+
+    Returns
+        : data frame = table retrieved from database
+    """
+    cols = get_col_names(conn, table)
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        data_list = cursor.fetchall()
+
+    return pd.DataFrame(data_list, columns=cols)
