af9440f626ec8a73895174b580fc5ac6b03e3248
Marshall Mykietyshyn <mmykietyshyn@gmail.com>
2023-01-12 12:32:08 +0100

    PyTorch module with custom functions

---
+++ models/common_torch.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = torch.from_numpy(X.astype(np.float32))
+        self.y = torch.from_numpy(y.astype(np.float32))
+        self.length = X.shape[0]
+
+    def __getitem__(self, idx):
+        return self.X[idx, :], self.y[idx]
+
+    def __len__(self):
+        return self.length
+
+
+# Define the model class
+class Regression_NN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Regression_NN, self).__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size)
+        )
+
+    def forward(self, x):
+        return self.linear_relu_stack(x)
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    # size = len(dataloader.dataset)
+    batch_loss = []
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred.squeeze(), y.squeeze())
+        batch_loss.append(loss.item())
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    return batch_loss
